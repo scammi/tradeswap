63992494c579c14855ab18ec47326d643d607990
scammi <scammi@gmail.com>
2024-02-02 11:54:39 -0300

    Compile

--- cross-chain/L2-counter/contracts/DestinationNFT.sol
+++ cross-chain/L2-counter/contracts/DestinationNFT.sol
@@ -2,15 +2,13 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DestinationNFT is ERC721, Ownable {
-    constructor(address initialOwner)
+contract DestinationNFT is ERC721 {
+    constructor()
         ERC721("Destination", "DST")
-        Ownable(initialOwner)
     {}
 
-    function safeMint(address to, uint256 tokenId) public onlyOwner {
+    function safeMint(address to, uint256 tokenId) public {
         _safeMint(to, tokenId);
     }
 }
